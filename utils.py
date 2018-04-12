83bca2f39240a0054f7fa59cbe4f05230695d555
TimCoding <timbvho@gmail.com>
2018-04-11 22:21:10 -0500

    ip address fix

--- utils.py
+++ utils.py
@@ -7,12 +7,13 @@ def runIfconfig():
 	#command = "ifconfig | grep -Eo -m 1 '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1"
 	ifcon = check_output(['ifconfig'])
 	ifconResult = ifcon.decode("utf-8");
-	regex = re.compile(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+	#regex = re.compile(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+	regex = re.compile(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.')
 	ipaddress = regex.search(ifconResult)
 	return ipaddress.group(0)
 
 def runNMAP():
-	ipaddress = runIfconfig() + "-30";
+	ipaddress = runIfconfig() + "1-30";
 	# print(ipaddress)
 	nmapResult = check_output(['nmap', ipaddress])
 	print(nmapResult.decode("utf-8"))
