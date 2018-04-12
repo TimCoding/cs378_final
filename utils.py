c4ad9fe9ed70f1efd3bffc947dd2f8815a644721
TimCoding <timbvho@gmail.com>
2018-04-11 21:05:56 -0500

    fix

--- utils.py
+++ utils.py
@@ -4,5 +4,5 @@ from subprocess import check_output
 def runIfconfig():
 	command = "ifconfig | grep -Eo -m 1 '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1"
 	commandArray =  command.split(' ')
-	result = check_output(commandArray).decode("utf-8")
+	result = check_output(command).decode("utf-8")
 	print(result)
