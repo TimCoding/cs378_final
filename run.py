eb807df044539ad5c2026e4500ab6762a387b88c
TimCoding <timbvho@gmail.com>
2018-04-11 20:59:13 -0500

    Automate 1

---
+++ run.py
@@ -0,0 +1,9 @@
+from utils import *
+
+#Main driver program for now
+#Will eventually replace once GUI is hooked up
+#Meant for testing only
+
+if __name__ == "__main__":
+	#Do shit here
+	runIfconfig()
---
+++ utils.py
@@ -0,0 +1,8 @@
+from subprocess import check_output
+
+#Runs ifconfig and gets ip address so NMAP can be performed
+def runIfconfig():
+	command = "ifconfig | grep -Eo -m 1 '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1"
+	commandArray =  command.split(' ')
+	result = check_output(commandArray).decode("utf-8")
+	print(result)
